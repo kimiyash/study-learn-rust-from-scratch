3650dcb256a265eb63a7b1f845ad1a3b81c2995f
kimiyash <kimiyash@gmail.com>
2024-10-11 08:23:47 +0900

    commit

--- scratch/examples/ch3-4-2-1.rs
+++ scratch/examples/ch3-4-2-1.rs
@@ -1,4 +1,30 @@
+#[derive(Debug)]
+struct XY {
+    x: Vec<i32>,
+    y: Vec<i32>,
+}
+
 fn main() {
+    // 構造体の例
+    let mut xy = XY {
+        x: vec![1, 2, 3],
+        y: Vec::new(),
+    };
+    // xy のフィールド x を借用すると、xy全体も借用されるのでコンパイルエラー、とならない…
+    for elm in xy.x.iter() {
+        xy.y.push(*elm * *elm);
+    }
+    println!("{:?}", xy);
+    // コンパイルエラーとなる場合下記のように書くといいらしい
+    let XY { x, y} = &mut xy;
+    for elm in x {
+        y.push(*elm * *elm);
+    }
+    println!("{:?}", xy);
+
+
+    
+    // 変数の例
     let mut a = 10;
     let b = &mut a;
     // if a == 10 { // ここでエラー
