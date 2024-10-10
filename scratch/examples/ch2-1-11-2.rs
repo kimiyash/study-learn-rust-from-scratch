cbdc6ece67200ff17f6de8a4afd46b838de4cc21
kimiyash <kimiyash@gmail.com>
2024-10-10 16:01:34 +0900

    commit

---
+++ scratch/examples/ch2-1-11-2.rs
@@ -0,0 +1,14 @@
+struct Buffer<const S: usize> {
+    buf: [u8; S],
+}
+
+fn main() {
+    // let buf = Buffer::<128> { buf: [0; 127] };
+    //     error[E0308]: mismatched types
+    //     --> examples/ch2-1-11-2.rs:6:36
+    //      |
+    //    6 |     let buf = Buffer::<128> { buf: [0; 127] };
+    //      |                                    ^^^^^^^^ expected an array with a fixed size of 128 elements, found one with 127 elements
+   
+    let buf = Buffer::<128> { buf: [0; 128] };   
+}
