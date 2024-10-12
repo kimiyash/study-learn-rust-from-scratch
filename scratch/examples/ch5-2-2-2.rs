38518493229f732e8114e432fb0de771dc746c1d
kimiyash <kimiyash@gmail.com>
2024-10-12 15:28:45 +0900

    commit

--- scratch/examples/ch5-2-2-2.rs
+++ scratch/examples/ch5-2-2-2.rs
@@ -29,4 +29,28 @@ mod b {
     }
 }
 
+mod c {
+    mod c_1_outer {
+        pub mod c_1_inner {
+            pub(crate) struct TypeC1; // 同じクレート内からのみ見える
+            pub(super) struct TypeC2; // 親モジュールからのみ見える
+            pub(in crate::c::c_1_outer) struct TypeC3; // 親モジュールからのみ見える
+            pub(self) struct TypeC4; // プライベートと同義
+        }
+        fn f() {
+            let p1 = c_1_inner::TypeC1;
+            let p2 = c_1_inner::TypeC2;
+            let p3 = c_1_inner::TypeC3;
+            // let p4 = c_1_inner::TypeC4; // プライベートなので見えない
+        }
+    }
+
+    fn g() {
+        let p1 = c_1_outer::c_1_inner::TypeC1;
+        // let p2 = c_1_outer::c_1_inner::TypeC2; // エラー
+        // let p3 = c_1_outer::c_1_inner::TypeC3; // エラー
+        // let p4 = c_1_outer::c_1_inner::TypeC4; // エラー
+}
+}
+
 fn main() {}
