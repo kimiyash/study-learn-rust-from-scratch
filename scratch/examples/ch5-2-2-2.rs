edb845900b15f95077b495e9507585a717a42212
kimiyash <kimiyash@gmail.com>
2024-10-12 15:19:41 +0900

    commit

---
+++ scratch/examples/ch5-2-2-2.rs
@@ -0,0 +1,33 @@
+mod b {
+    pub struct TypeB;
+    mod b_1 {
+        pub struct TypeB1 {
+            pub n: usize,
+            m: usize,
+        }
+
+        impl TypeB1 {
+            fn g(&self) {}
+            pub fn h(&self) {}
+        }
+
+        fn f1(p: &super::b_1::TypeB1) {
+            println!("{}", p.n);
+            println!("{}", p.m);
+            p.g();
+            p.h();
+        }
+    }
+
+    pub mod b_2{
+        fn f2(p: &super::b_1::TypeB1) {
+            println!("{}", p.n);
+            // println!("{}", p.m); // m はプライベート
+            // p.g(); // g はプライベート
+            p.h();
+        }
+    }
+}
+
+
+fn main() {}
