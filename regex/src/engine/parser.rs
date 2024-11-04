ccacc2f4d935ac569cb0288fe8d1842a51c2a0aa
kimiyash <kimiyash@gmail.com>
2024-11-04 16:24:26 +0900

    parser

--- regex/src/engine/parser.rs
+++ regex/src/engine/parser.rs
@@ -4,3 +4,46 @@ use std::{
     fmt::{self, Display}
     mem::take
 };
+
+/// 抽象構文木を表現するための型
+#[derive(Debug)]
+pub enum AST {
+    Char(char),
+    Plus(Box<AST>),
+    Star(Box<AST>),
+    Question(Box<AST>),
+    Or(Box<AST>, Box<AST>),
+    Seq(Vec<AST>),
+}
+
+#[derive(Debug)]
+pub enum ParserError {
+    InvalidEscape(usize, char), // 誤ったエスケープシーケンス
+    InvalidRightParen(usize),   // 開き括弧なし
+    NoPrev(usize),              // +, |, *, ? の前に式がない
+    NoRightParen,               // 閉じ括弧なし
+    Empty,                      // 空のパターン
+}
+
+/// パースエラーを表示するために、Displayトレイトを実装
+impl Display for ParserError {
+    fn fmt(&self, &mut :fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ParserError::InvalidEscape(pos, c) => {
+                write!(f, "ParseError: invalid escape: pos = {pos}, char = `{c}`")
+            }
+            ParseError::InvalidRightParen(pos) => {
+                write!(f, "ParseError: invalid right parenthesis: pos = {pos}")
+            }
+            ParseError::NoPrev(pos) => {
+                write!(f, "ParseError: no previous expression: pos = {pos}")
+            }
+            ParseError::NoRightParen => {
+                write!(f, "ParseError: no right parenthesis}")
+            }
+            ParseError::Empty => write!(f, "ParseError: empty expression"),
+        }
+    }
+}
+
+impl Error for ParseError {}
